566a417a467447c6b651ba3db0fd5d51705c45f2
UxoriusTen <133169726+UxoriusTen@users.noreply.github.com>
2023-11-04 11:12:54 -0500

    remove a leftover array

--- Challenge-01/app.js
+++ Challenge-01/app.js
@@ -2,42 +2,35 @@ const fs = require('fs');
 const message = fs.readFileSync('text.txt','utf-8')
 
 function findPatron (message){
-  let words = message.split(' ')
   let array = message.split(' ')
-  for(let i = 0;i<words.length;i++){
-    words[i] = words[i].toLowerCase()
+  for(let i = 0;i<array.length;i++){
     array[i] = array[i].toLowerCase()
   }
-  //No duplicates
 
-  let nodu = words.filter((item,index)=>{
-    return words.indexOf(item)===index
+  //No duplicates
+  let nodu = array.filter((item,index)=>{
+    return array.indexOf(item)===index
   })
 
-  console.log(nodu);
-  //count
+  //create the array that count how many times a word repeats
   let count = [];
   for(let i = 0;i<nodu.length;i++){
     count[i]=0
   }
+  // count
   nodu.forEach((element,index)=>{
     for(let i = 0;i<array.length;i++){
       if(element == array[i]){count[index]++}
     }
   })
-  // console.log(count);
   //write patron
   let patron = ''
   for(let i = 0;i<nodu.length ;i++){
-    patron=patron + words[i] + count[i]
+    patron=patron + nodu[i] + count[i]
   }
-  // console.log(patron);
+  // write the answer in patron.txt file
   fs.writeFileSync('patron.txt',patron)
   // return patron
 }
 
-// const patron = findPatron(message)
 findPatron(message)
-// console.log(patron);
-
-// console.log(words);
