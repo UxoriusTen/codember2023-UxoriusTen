de194ce007e2a2bf1e7de80f707f88b9856fe81d
UxoriusTen <133169726+UxoriusTen@users.noreply.github.com>
2023-11-11 01:33:55 -0500

    cleaning the code

--- Challenge-02/app.js
+++ Challenge-02/app.js
@@ -1,32 +1,34 @@
+function compiler(code){
 
-async function decode(steps){
-	try{
+	let symbols = code.split('')
+	let initVal = 0
+	let resolve = ''
 
-		steps = await fetch('https://codember.dev/data/message_02.txt')
-		steps = await steps.text()
-		console.log(steps);
+	/* 
+	"#" Incrementa el valor numérico en 1.
+	"@" Decrementa el valor numérico en 1.
+	"*" Multiplica el valor numérico por sí mismo.
+	"&" Imprime el valor numérico actual.
+	*/
 
-		let symbols = steps.split('')
-		let initVal = 0
-		let resolve = ''
-
-		/* 
-		"#" Incrementa el valor numérico en 1.
-		"@" Decrementa el valor numérico en 1.
-		"*" Multiplica el valor numérico por sí mismo.
-		"&" Imprime el valor numérico actual.
-		*/
-		for (let i = 0;i<symbols.length;i++){
-			if(symbols[i]=='#'){initVal++}
-			else if(symbols[i]=='@'){initVal--}
-			else if(symbols[i]=='*'){initVal=initVal**2}
-			else if(symbols[i]=='&'){resolve=resolve+initVal}
-		}
+	for (let i = 0;i<symbols.length;i++){
+		if(symbols[i]=='#'){initVal++}
+		else if(symbols[i]=='@'){initVal--}
+		else if(symbols[i]=='*'){initVal=initVal**2}
+		else if(symbols[i]=='&'){resolve=resolve+initVal}
+	}
 
 		console.log(resolve);
-		
+}
 
-		
+
+
+async function decode(){
+	try{
+		processes = await (await fetch('https://codember.dev/data/message_02.txt')).text()
+
+		compiler(processes)
+	
 	}catch(e){console.log(e);}
 }
 
